f0e21548c8d27b6026e41849e1349c3d5cb42e2d
UndeadTeam <contato@undeadbrasil.com>
2016-06-24 01:41:48 -0300

    Add novas coisas ao Exile

---
+++ Exile.bat
@@ -0,0 +1,5 @@
+arma3server.exe  -port=2302 "-config=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\TADST\exile\TADST_config.cfg" "-cfg=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\TADST\wastelandStratis\TADST_basic.cfg" "-profiles=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\TADST\exile" -name=exile -filePatching "-mod=@ExileServer;@extDB2;@Ryanzombies;@Exile;"
+
+echo ARMA 3 Server has started.
+
+exit
